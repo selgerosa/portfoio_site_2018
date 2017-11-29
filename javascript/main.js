a24fe1a1060fd8bbc1e62a8acd5a2e9da59367b3
Selina <31773266+selinagerosa@users.noreply.github.com>
2017-11-28 21:09:45 -0500

    fix lightbox bgs

--- javascript/main.js
+++ javascript/main.js
@@ -9,3 +9,44 @@
 // Both parameters must be in quotes.
 
 // toggleClass('.myClass', 'class-to-toggle')
+
+function bgchange(m) {  //m is the # modal
+  var lst = ["url(img/bgs/sky1.gif)","url(img/bgs/sky2.gif)","url(img/bgs/sky3.gif)","url(img/bgs/sky4.gif)","url(img/bgs/sky5.gif)","url(img/bgs/sky6.gif)","url(img/bgs/sky7.gif)", "url(img/bgs/sky8.gif)"]
+  var rand = Math.floor((Math.random() * 8));
+  document.getElementById('modal'+m).style.backgroundImage = lst[rand];
+}
+function openbox(m) {
+  document.getElementById('modal'+m).style.display = "block";
+}
+
+function closebox(m) {
+  document.getElementById('modal'+m).style.display = "none";
+}
+
+var ind = 1;
+showSlides(ind,1);
+
+function plusSlides(n,m) {
+  showSlides(ind += n,m);
+}
+
+function current(n, m) {
+  showSlides(ind = n, m);
+}
+
+function showSlides(n, m) {
+  var i;
+  var slides = document.getElementsByClassName("slides"+m);
+  var dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) {ind = 1}
+  if (n < 1) {ind = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[ind-1].style.display = "block";
+  dots[ind-1].className += " active";
+}
