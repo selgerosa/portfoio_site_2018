30b63ae7ab00734a80de286ad96c1eda2cabaa50
Selina <31773266+selinagerosa@users.noreply.github.com>
2018-07-02 11:10:09 -0400

    takin out the mini sites, gotta fix the links now tho

--- weather/javascript/main.js
+++
@@ -1,9 +0,0 @@
-function gen (lst) {
-  var rand = Math.floor((Math.random() * 10))
-  document.getElementById('bg').style.backgroundImage = lst[rand]
-}
-
-function generror (lst) {
-  var rand = Math.floor((Math.random() * 10))
-  document.getElementById('error').style.backgroundImage = lst[rand]
-}
